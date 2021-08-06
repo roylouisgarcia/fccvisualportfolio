56d3836f38d1fc7c7d88d694a4b28a9a15645fe7
Roy Garcia <roygarcia201@gmail.com>
2021-08-05 21:25:06 -0700

    added a progress bar and sync it when the panel is clicked. ttd is to sync when progress circle or buttons are clicked

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const panels = document.querySelectorAll('.panel')
 
+let currentActive = 1
+
 panels.forEach((panel) => {
 	panel.addEventListener('click', () => {
 		removeActiveClasses()
 		panel.classList.add('active')
+		syncCircleProgressActive()
 	})
 })
 
@@ -11,6 +14,20 @@ function removeActiveClasses() {
 	panels.forEach(panel => {
 		panel.classList.remove('active')
 	})
+}
+
+function syncCircleProgressActive() {
+	let activePanelIndex = 1
+	let counter = 0
+	panels.forEach(panel => {
+		counter++
+		if (panel.classList.contains('active')){
+			activePanelIndex = counter
+		}
+	})
+	console.log(activePanelIndex)
+	currentActive = activePanelIndex
+	update()
 }
 
 
@@ -29,3 +46,55 @@ function removeActiveClasses() {
 //       var x = window.matchMedia("(max-width: 480px)")
 //       myFunction(x) // Call listener function at run time
 //       x.addListener(myFunction) // Attach listener function on state changes 
+
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+
+next.addEventListener('click', () => {
+	currentActive++
+
+	if(currentActive > circles.length){
+		currentActive = circles.length
+	}
+	console.log(currentActive)
+
+	update() 
+
+})
+
+prev.addEventListener('click', () => {
+	currentActive--
+
+	if(currentActive < 1 ){
+		currentActive = 1
+	}
+	console.log(currentActive)
+	update() 
+
+})
+
+function update(){
+	circles.forEach((circle, idx) => {
+		if(idx < currentActive){
+			circle.classList.add('ciractive')
+		} else {
+			circle.classList.remove('ciractive')
+		}
+	})
+
+	const ciractives = document.querySelectorAll('.ciractive')
+	progress.style.width = (ciractives.length -1 ) / (circles.length -1 ) * 100 + '%'
+	console.log(ciractives.length, circles.length)
+
+	if(currentActive === 1){
+		prev.disabled = true
+	} else if (currentActive === circles.length){
+		next.disabled = true
+	} else {
+		prev.disabled = false
+		next.disabled = false
+	}
+}
